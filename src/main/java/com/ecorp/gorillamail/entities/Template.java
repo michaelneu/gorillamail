16fb0b675d843f522a5dd42b912c773097975c53
Michael Neu <michaelneu@users.noreply.github.com>
2018-01-05 20:24:05 +0100

    Fixes #1

--- src/main/java/com/ecorp/gorillamail/entities/Template.java
+++ src/main/java/com/ecorp/gorillamail/entities/Template.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -33,6 +34,7 @@ public class Template extends AbstractLongEntity {
     private String name;
 
     @Column( nullable = false )
+    @Lob
     @Getter
     @Setter
     private String body;
